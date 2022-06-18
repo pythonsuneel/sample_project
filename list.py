c9f8dcc681e5c9b0e45ce85e7e9f3dc76f2e968c
suneelchitteti <suneelchitteti695@gmail.com>
2022-06-18 11:08:57 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,12 @@
+import os
+list1=[8,9,3,6,1,10]
+list1.reverse()
+print("the reverse list is:",list1)
+list2=[34,54,65,35,78,98,90,345,65,478,108]
+list2.sort()
+print("the sorted list is:",list2)
+list3=[]
+list3=list1.copy()
+print("the copied list is:",list3)
+index_value=list2[2:6]
+print("the index value are:",index_value)
